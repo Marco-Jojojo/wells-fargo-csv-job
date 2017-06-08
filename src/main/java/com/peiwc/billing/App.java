ffd6cf5d38aa3a5fc2db34932ff74f3dd916b3ff
Carlos Alberto Acosta Torres <carlos.acosta@softtekclients.com>
2017-06-08 15:54:07 -0700

    initial import

---
+++ src/main/java/com/peiwc/billing/App.java
@@ -0,0 +1,13 @@
+package com.peiwc.billing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
