e6fec68079c6d7bce8ede19baeada10a9d1c9f38
carlos.acosta <carlos.acosta@softtek.com>
2017-06-12 15:09:40 -0700

    added log4j configuration

--- src/main/java/com/peiwc/billing/App.java
+++ src/main/java/com/peiwc/billing/App.java
@@ -1,7 +1,9 @@
 package com.peiwc.billing;
 
+import org.apache.log4j.xml.DOMConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.w3c.dom.DOMConfiguration;
 
 import com.peiwc.billing.configuration.ConfigurationBean;
 
@@ -13,10 +15,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         
+    	DOMConfigurator.configure("wells-fargo-log4j.xml");
         ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigurationBean.class);
-
+        
         
     }
 }
