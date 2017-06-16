e342f9162d2f5d838d70c09eaf7f7658a0cfe30b
carlos.acosta <carlos.acosta@softtek.com>
2017-06-16 16:54:39 -0700

    Added missing javadocs.

--- src/main/java/com/peiwc/billing/process/ProcessState.java
+++ src/main/java/com/peiwc/billing/process/ProcessState.java
@@ -5,22 +5,40 @@ package com.peiwc.billing.process;
  */
 public enum ProcessState {
 
-    FINISHED("FINISHED"), PENDING_START("PENDING_START"), RUNNING("RUNNING"), ALREADY_RUN("ALREADY_RUN"), IO_ERROR("IO_ERROR");
+	/**
+	 * sets the process as finished
+	 */
+	FINISHED("FINISHED"),
+	/**
+	 * sets the process as pending start
+	 */
+	PENDING_START("PENDING_START"),
+	/**
+	 * sets the process as running
+	 */
+	RUNNING("RUNNING"),
+	/**
+	 * sets the process as already run
+	 */
+	ALREADY_RUN("ALREADY_RUN"),
+	/**
+	 * sets the process to io error, when an exception writing occurs.
+	 */
+	IO_ERROR("IO_ERROR");
 
-    private final String state;
+	private final String state;
 
-    private ProcessState(final String state) {
-        this.state = state;
-    }
+	private ProcessState(final String state) {
+		this.state = state;
+	}
 
-    /**
-     * gets the name of current state.
-     *
-     * @return name of current state.
-     */
-    public String getName() {
-        return this.state;
-    }
-
+	/**
+	 * gets the name of current state.
+	 *
+	 * @return name of current state.
+	 */
+	public String getName() {
+		return this.state;
+	}
 
 }
--- src/main/java/com/peiwc/billing/process/mail/MailSender.java
+++ src/main/java/com/peiwc/billing/process/mail/MailSender.java
@@ -1,8 +1,6 @@
 package com.peiwc.billing.process.mail;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -10,7 +8,10 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by carlos.acosta on 16/06/2017.
@@ -18,41 +19,41 @@ import java.util.Properties;
 @Component
 public class MailSender {
 
-    private static final String DEFAULT_SMTP_PORT = "25";
+	private static final String DEFAULT_SMTP_PORT = "25";
 
-    private static final Logger LOGGER = Logger.getLogger(MailSender.class);
+	private static final Logger LOGGER = Logger.getLogger(MailSender.class);
 
-    @Value("${mail.send.enabled}")
-    private boolean isEnabled;
+	@Value("${mail.send.enabled}")
+	private boolean isEnabled;
 
-    @Value("${mail.to.subject}")
-    private String mailSubject;
+	@Value("${mail.to.subject}")
+	private String mailSubject;
 
-    /**
-     * sends a mail message indicating the finish status of the current process.
-     *
-     * @param mailMessage message argument that contains the body text.
-     * @throws MessagingException if message could not be sent.
-     */
-    public void sendMailMessage(final String mailMessage) {
-        if (isEnabled) {
-            try {
-                final Properties properties = new Properties();
-                final String smtpHost = System.getProperty("email.server.address");
-                properties.setProperty("mail.smtp.auth", "false");
-                properties.setProperty("mail.smtp.host", smtpHost);
-                properties.setProperty("mail.smtp.port", DEFAULT_SMTP_PORT);
-                final Session session = Session.getInstance(properties);
-                final Message message = new MimeMessage(session);
-                final String responsible = System.getProperty("email.notification.address");
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(responsible));
-                message.setSubject(mailSubject);
-                message.setText(mailMessage);
-                Transport.send(message);
-            } catch (final MessagingException ex) {
-                LOGGER.error(ex, ex);
-            }
-        }
-    }
+	/**
+	 * sends a mail message indicating the finish status of the current process.
+	 *
+	 * @param mailMessage
+	 *            message argument that contains the body text.
+	 */
+	public void sendMailMessage(final String mailMessage) {
+		if (isEnabled) {
+			try {
+				final Properties properties = new Properties();
+				final String smtpHost = System.getProperty("email.server.address");
+				properties.setProperty("mail.smtp.auth", "false");
+				properties.setProperty("mail.smtp.host", smtpHost);
+				properties.setProperty("mail.smtp.port", MailSender.DEFAULT_SMTP_PORT);
+				final Session session = Session.getInstance(properties);
+				final Message message = new MimeMessage(session);
+				final String responsible = System.getProperty("email.notification.address");
+				message.setRecipient(Message.RecipientType.TO, new InternetAddress(responsible));
+				message.setSubject(mailSubject);
+				message.setText(mailMessage);
+				Transport.send(message);
+			} catch (final MessagingException ex) {
+				MailSender.LOGGER.error(ex, ex);
+			}
+		}
+	}
 
 }
