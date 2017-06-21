63547c290e26f9937f9f1fd52ba0b59bf12985f5
carlos.acosta <carlos.acosta@softtek.com>
2017-06-21 14:39:30 -0700

    Removed format exceptions when running principal scripts for creating tables in h2

--- src/main/java/com/peiwc/billing/process/MainProcess.java
+++ src/main/java/com/peiwc/billing/process/MainProcess.java
@@ -1,21 +1,18 @@
 package com.peiwc.billing.process;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.peiwc.billing.App;
+import com.peiwc.billing.domain.WFMamOpHDRTRLR;
+import com.peiwc.billing.process.billingP1.WFMamSrcGenRecs;
+import com.peiwc.billing.process.mail.MailSender;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.peiwc.billing.App;
-import com.peiwc.billing.domain.WFMamOpHDRTRLR;
-
-import com.peiwc.billing.process.billingP1.CalcUnclearedBilledAmt;
-import com.peiwc.billing.process.billingP1.WFMamSrcGenRecs;
-import com.peiwc.billing.process.mail.MailSender;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -25,89 +22,90 @@ import com.peiwc.billing.process.mail.MailSender;
 @Component("mainProcess")
 public class MainProcess {
 
-	private static final Logger LOGGER = Logger.getLogger(MainProcess.class);
+    private static final Logger LOGGER = Logger.getLogger(MainProcess.class);
 
-	@Autowired
-	private ProcessManagerCheck processManagerCheck;
+    @Autowired
+    private ProcessManagerCheck processManagerCheck;
 
-	@Autowired
-	private WFMamOpHDRTRLRProcess wfMamOpHDRTRLRProcess;
+    @Autowired
+    private WFMamOpHDRTRLRProcess wfMamOpHDRTRLRProcess;
 
-	@Autowired
-	private WriteWFMAMSrcFileCSV writeWFMAMSrcFileCSV;
-	
-	@Autowired
-	private WFMamSrcGenRecs wfMamSrcGenRecs;
+    @Autowired
+    private WriteWFMAMSrcFileCSV writeWFMAMSrcFileCSV;
 
-	@Value("${csv.name.suffix}")
-	private String dateFormatPattern;
+    @Autowired
+    private WFMamSrcGenRecs wfMamSrcGenRecs;
 
-	@Autowired
-	private MailSender mailSender;
+    @Value("${csv.name.suffix}")
+    private String dateFormatPattern;
 
-	/**
-	 * this is the main process that checks if the process has already run and
-	 * then writes the entire data from a cycle to database.
-	 *
-	 * @return a flag indicating the process has been run successfully
-	 */
-	public boolean runWellsFargoCSVProcess() {
-		boolean hasRunSuccessfully = true;
-		final Date currentDate = new Date();
-		int nextCycle = 0;
-		if (!processManagerCheck.checkIfProcessHasAlreadyRun(currentDate)) {
-			try {
-				nextCycle = processManagerCheck.getNextCycleNumber();
-				MainProcess.LOGGER.info("Process Has not been run, Next cycle is: " + nextCycle);
-				final WFMamOpHDRTRLR wfMamOpHDRTRLR = wfMamOpHDRTRLRProcess.saveNextCycle(nextCycle, currentDate);
-				wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.PENDING_START, nextCycle);
-				MainProcess.LOGGER.info("wfMamOpHDRTRLR cycleNumber: " + wfMamOpHDRTRLR.getCycleNumber()
-						+ " , creationDate:" + wfMamOpHDRTRLR.getCreationDate());
-				wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.RUNNING, nextCycle);
-				// here goes the main process where the data for WF_MAM_SRC_FILE
-				// table is filled.
-				this.wfMamSrcGenRecs.billingProcess(nextCycle);
-				final String fileNameId = System.getProperty("csv.id.prefix");
-				final String fileNamePrefix = System.getProperty("csv.name.prefix");
-				final String fileNameLocation = System.getProperty("csv.path.location");
-				String fileName = "test.csv";
-				if (StringUtils.isNotEmpty(fileNamePrefix) && StringUtils.isNotEmpty(fileNameId)) {
-					fileName = fileNameId + "_" + fileNamePrefix + generateFileSuffix() + ".csv";
-				}
-				try {
-					final int totalRecordCount = writeWFMAMSrcFileCSV.writeDataToCSV(nextCycle, fileName);
-					wfMamOpHDRTRLRProcess.saveTotalRecordsProcessed(nextCycle, totalRecordCount);
-					wfMamOpHDRTRLRProcess.moveGeneratedFileToExternalLocation(fileName, fileNameLocation);
-				} catch (final IOException ex) {
-					MainProcess.LOGGER.error(ex, ex);
-					hasRunSuccessfully = false;
-					wfMamOpHDRTRLRProcess.saveErrorMessage(nextCycle, ex.getMessage());
-				}
-				if (hasRunSuccessfully) {
-					wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.FINISHED, nextCycle);
-					mailSender.sendMailMessage("Process #" + nextCycle + "has run successfully ");
-				}
-			} catch (final Exception ex) {
-				wfMamOpHDRTRLRProcess.saveErrorMessage(nextCycle, ex.getMessage());
-				mailSender.sendMailMessage("Process # " + nextCycle + " , has failed: " + ex.getMessage());
-			}
-		} else {
-			MainProcess.LOGGER.info("Process has already run today");
-			hasRunSuccessfully = false;
-			wfMamOpHDRTRLRProcess.setProcessAsAlreadyRunForToday();
-		}
-		return hasRunSuccessfully;
-	}
+    @Autowired
+    private MailSender mailSender;
 
-	private void fillTables(final int nextCycle) {
-		// TODO: generate here the process to fill the tables with data.
+    /**
+     * this is the main process that checks if the process has already run and
+     * then writes the entire data from a cycle to database.
+     *
+     * @return a flag indicating the process has been run successfully
+     */
+    public boolean runWellsFargoCSVProcess() {
+        boolean hasRunSuccessfully = true;
+        final Date currentDate = new Date();
+        int nextCycle = 0;
+        if (!processManagerCheck.checkIfProcessHasAlreadyRun(currentDate)) {
+            try {
+                nextCycle = processManagerCheck.getNextCycleNumber();
+                MainProcess.LOGGER.info("Process Has not been run, Next cycle is: " + nextCycle);
+                final WFMamOpHDRTRLR wfMamOpHDRTRLR = wfMamOpHDRTRLRProcess.saveNextCycle(nextCycle, currentDate);
+                wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.PENDING_START, nextCycle);
+                MainProcess.LOGGER.info("wfMamOpHDRTRLR cycleNumber: " + wfMamOpHDRTRLR.getCycleNumber()
+                        + " , creationDate:" + wfMamOpHDRTRLR.getCreationDate());
+                wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.RUNNING, nextCycle);
+                // here goes the main process where the data for WF_MAM_SRC_FILE
+                // table is filled.
+                this.wfMamSrcGenRecs.billingProcess(nextCycle);
+                final String fileNameId = System.getProperty("csv.id.prefix");
+                final String fileNamePrefix = System.getProperty("csv.name.prefix");
+                final String fileNameLocation = System.getProperty("csv.path.location");
+                String fileName = "test.csv";
+                if (StringUtils.isNotEmpty(fileNamePrefix) && StringUtils.isNotEmpty(fileNameId)) {
+                    fileName = fileNameId + "_" + fileNamePrefix + generateFileSuffix() + ".csv";
+                }
+                try {
+                    final int totalRecordCount = writeWFMAMSrcFileCSV.writeDataToCSV(nextCycle, fileName);
+                    wfMamOpHDRTRLRProcess.saveTotalRecordsProcessed(nextCycle, totalRecordCount);
+                    wfMamOpHDRTRLRProcess.moveGeneratedFileToExternalLocation(fileName, fileNameLocation);
+                } catch (final IOException ex) {
+                    MainProcess.LOGGER.error(ex, ex);
+                    hasRunSuccessfully = false;
+                    wfMamOpHDRTRLRProcess.saveErrorMessage(nextCycle, ex.getMessage());
+                }
+                if (hasRunSuccessfully) {
+                    wfMamOpHDRTRLRProcess.setCurrentState(ProcessState.FINISHED, nextCycle);
+                    mailSender.sendMailMessage("Process #" + nextCycle + "has run successfully ");
+                }
+            } catch (final Exception ex) {
+                hasRunSuccessfully = false;
+                wfMamOpHDRTRLRProcess.saveErrorMessage(nextCycle, ex.getMessage());
+                mailSender.sendMailMessage("Process # " + nextCycle + " , has failed: " + ex.getMessage());
+            }
+        } else {
+            MainProcess.LOGGER.info("Process has already run today");
+            hasRunSuccessfully = false;
+            wfMamOpHDRTRLRProcess.setProcessAsAlreadyRunForToday();
+        }
+        return hasRunSuccessfully;
+    }
 
-	}
+    private void fillTables(final int nextCycle) {
+        // TODO: generate here the process to fill the tables with data.
 
-	private String generateFileSuffix() {
-		final Date date = new Date();
-		final SimpleDateFormat sdf = new SimpleDateFormat(dateFormatPattern);
-		return sdf.format(date);
-	}
+    }
+
+    private String generateFileSuffix() {
+        final Date date = new Date();
+        final SimpleDateFormat sdf = new SimpleDateFormat(dateFormatPattern);
+        return sdf.format(date);
+    }
 
 }
