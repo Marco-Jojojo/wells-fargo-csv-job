d514249d812f0bcaa94ef998e360ceb5b50a4653
carlos.acosta <carlos.acosta@softtek.com>
2017-06-22 15:35:16 -0700

    Removed Append stream from output file

--- src/main/java/com/peiwc/billing/process/WriteWFMAMSrcFileCSV.java
+++ src/main/java/com/peiwc/billing/process/WriteWFMAMSrcFileCSV.java
@@ -89,7 +89,7 @@ public class WriteWFMAMSrcFileCSV {
 		OutputStream out = null;
 		final Set<WFMamSrcFilePK> errorList = this.getErrorsFromCycle(cycleNumber);
 		try {
-			out = new FileOutputStream(fileName, true);
+			out = new FileOutputStream(fileName);
 			final byte[] headers = (csvHeaders + WriteWFMAMSrcFileCSV.NEW_LINE).getBytes();
 			out.write(headers);
 			for (final WFMamSrcFile wfMamSrcFile : wfList) {
