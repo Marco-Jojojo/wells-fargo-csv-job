55862dcd0352a476b6eff7dee198c65345026872
carlos.acosta <carlos.acosta@softtek.com>
2017-06-23 20:43:53 -0700

    changed to default jdbc mssql driver

--- src/main/java/com/peiwc/billing/App.java
+++ src/main/java/com/peiwc/billing/App.java
@@ -2,9 +2,10 @@ package com.peiwc.billing;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
+import com.peiwc.billing.configuration.ConfigurationBean;
 import com.peiwc.billing.process.MainProcess;
 
 /**
@@ -23,12 +24,12 @@ public class App {
 	public static void main(final String[] args) {
 		DOMConfigurator.configure("wells-fargo-log4j.xml");
 		try {
-			// final AbstractApplicationContext ctx = new
-			// AnnotationConfigApplicationContext(ConfigurationBean.class);
-			final ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+			final AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigurationBean.class);
+			// final ApplicationContext ctx = new
+			// ClassPathXmlApplicationContext("applicationContext.xml");
 			final MainProcess mainProcess = ctx.getBean(MainProcess.class);
 			mainProcess.runWellsFargoCSVProcess();
-			// ctx.close();
+			ctx.close();
 		} catch (final Exception e) {
 			App.LOGGER.error(e, e);
 		}
