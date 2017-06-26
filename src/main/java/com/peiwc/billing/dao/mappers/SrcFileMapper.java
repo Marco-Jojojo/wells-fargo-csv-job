c472c6eaca5e7812ea767a67ca9e35784a3d23c5
Manuel Solano <jose.solano@softtek.com>
2017-06-25 21:59:25 -0700

    create mapper for Apply credits

--- src/main/java/com/peiwc/billing/dao/mappers/SrcFileMapper.java
+++ src/main/java/com/peiwc/billing/dao/mappers/SrcFileMapper.java
@@ -1,12 +1,12 @@
-package com.peiwc.billing.process.billing1;
+package com.peiwc.billing.dao.mappers;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import com.peiwc.billing.domain.WFMamSrcFile;
-
 import com.peiwc.billing.domain.WFMamSrcFilePK;
 
 public class SrcFileMapper implements RowMapper<WFMamSrcFile> {
@@ -15,6 +15,13 @@ public class SrcFileMapper implements RowMapper<WFMamSrcFile> {
 	public WFMamSrcFile mapRow(final ResultSet rs, final int rowNumber) throws SQLException {
 		final WFMamSrcFile wfMamSrcFile = new WFMamSrcFile();
 		final WFMamSrcFilePK id = new WFMamSrcFilePK();
+		final ResultSetMetaData metaData = rs.getMetaData();
+		for (int i = 0; i < metaData.getColumnCount(); i++) {
+			final String nameColumn = metaData.getColumnName(i + 1);
+			if (nameColumn.equals("CYCLE_NUMBER")) {
+				id.setCycleNumber(rs.getInt("CYCLE_NUMBER"));
+			}
+		}
 		id.setSequenceNumber(rs.getInt("SEQUENCE_NUMBER"));
 		wfMamSrcFile.setId(id);
 		wfMamSrcFile.setReferenceNumber(Integer.toString(rs.getInt("REFERENCE_NUMBER")));
--- src/main/java/com/peiwc/billing/dao/mappers/SrcFileMapperForApplyCredits.java
+++ src/main/java/com/peiwc/billing/dao/mappers/SrcFileMapperForApplyCredits.java
@@ -6,14 +6,18 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.peiwc.billing.domain.WFMamSrcFile;
+import com.peiwc.billing.domain.WFMamSrcFilePK;
 
-public class SrcFileMapperForTwoYearsPolicies implements RowMapper<WFMamSrcFile> {
+public class SrcFileMapperForApplyCredits implements RowMapper<WFMamSrcFile> {
 
 	@Override
 	public WFMamSrcFile mapRow(final ResultSet rs, final int rowNumber) throws SQLException {
 		final WFMamSrcFile wfMamSrcFile = new WFMamSrcFile();
-		wfMamSrcFile.setReferenceNumber(Integer.toString(rs.getInt("REFERENCE_NUMBER")));
+		final WFMamSrcFilePK id = new WFMamSrcFilePK();
+		id.setCycleNumber(rs.getInt("CYCLE_NUMBER"));
+		wfMamSrcFile.setId(id);
 		wfMamSrcFile.setSecondaryAuth(Integer.toString(rs.getInt("SECONDARY_AUTH")));
+		wfMamSrcFile.setAmountDue(rs.getFloat("AMOUNT_DUE"));
 		return wfMamSrcFile;
 	}
 
