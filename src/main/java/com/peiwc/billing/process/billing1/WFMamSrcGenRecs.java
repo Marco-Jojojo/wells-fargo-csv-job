f45d544b927e7d3a0fcac2d858c49463241fcdcc
Manuel Solano <jose.solano@softtek.com>
2017-06-25 22:46:46 -0700

    modified database scripts on POLICY_MASTER table

--- src/main/java/com/peiwc/billing/process/billing1/WFMamSrcGenRecs.java
+++ src/main/java/com/peiwc/billing/process/billing1/WFMamSrcGenRecs.java
@@ -20,10 +20,10 @@ public class WFMamSrcGenRecs {
 
 	public void billingProcess(final int cycleNumber) {
 		this.calcUnclearedBilledAmt.updWFMamSrcFileRec(cycleNumber);
-		//this.calcUnclearedUnBilledCreditAmt.wfMamSrcFileUpdRecDBI1(cycleNumber);
-		//this.processPoliciesLessOrEqual2YearsOld.processPolicies(cycleNumber);
-		//this.applyCredits.applyCreditsProcess(cycleNumber);
-		
+		this.calcUnclearedUnBilledCreditAmt.wfMamSrcFileUpdRecDBI1(cycleNumber);
+		this.processPoliciesLessOrEqual2YearsOld.processPolicies(cycleNumber);
+		this.applyCredits.applyCreditsProcess(cycleNumber);
+
 	}
 
 }
