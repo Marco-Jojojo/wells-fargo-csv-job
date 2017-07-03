b61bd915d4eca51dced24215841325a91f95caee
carlos.acosta <carlos.acosta@softtek.com>
2017-07-03 09:37:18 -0700

    added mail-from property for sending mails

--- src/main/java/com/peiwc/billing/process/mail/MailSender.java
+++ src/main/java/com/peiwc/billing/process/mail/MailSender.java
@@ -2,6 +2,7 @@ package com.peiwc.billing.process.mail;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -18,16 +19,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MailSender {
-
 	private static final String DEFAULT_SMTP_PORT = "25";
-
 	private static final Logger LOGGER = Logger.getLogger(MailSender.class);
-
 	@Value("${mail.send.enabled}")
 	private boolean isEnabled;
-
 	@Value("${mail.to.subject}")
 	private String mailSubject;
+	@Value("${mail.send.from}")
+	private String mailFrom;
 
 	/**
 	 * sends a mail message indicating the finish status of the current process.
@@ -47,6 +46,8 @@ public class MailSender {
 				final Message message = new MimeMessage(session);
 				final String responsible = System.getProperty("email.notification.address");
 				message.setRecipient(Message.RecipientType.TO, new InternetAddress(responsible));
+				final Address[] from = new Address[] { new InternetAddress(mailFrom) };
+				message.addFrom(from);
 				message.setSubject(mailSubject);
 				message.setText(mailMessage);
 				Transport.send(message);
@@ -55,5 +56,4 @@ public class MailSender {
 			}
 		}
 	}
-
 }
