d468f58d9f3bf1bbce263502fc35b9ab2df83cb6
roberto.pech <alfonso.pech@softtek.com>
2017-07-18 12:09:43 -0700

    updating mapper file for user information

--- src/main/java/com/peiwc/billing/dao/mappers/WFUserInfoMapper.java
+++ src/main/java/com/peiwc/billing/dao/mappers/WFUserInfoMapper.java
@@ -15,11 +15,11 @@ public class WFUserInfoMapper implements RowMapper<WFUserInfo> {
 	@Override
 	public WFUserInfo mapRow(final ResultSet rs, final int rowNum) throws SQLException {
 		final WFUserInfo user = new WFUserInfo();
-		user.setAddress(rs.getString("ADDR_1"));
-		user.setAddress2(rs.getString("ADDR_2"));
-		user.setCity(rs.getString("CITY"));
-		user.setState(rs.getString("STATE"));
-		user.setZip(rs.getString("ZIP1"));
+		user.setAddress(rs.getString("INS_ADDR_1"));
+		user.setAddress2(rs.getString("INS_ADDR_2"));
+		user.setCity(rs.getString("INS_CITY"));
+		user.setState(rs.getString("INS_STATE_CODE"));
+		user.setZip(rs.getString("INS_ZIP_1"));
 		user.setStatus(rs.getString("STATUS_CODE"));
 		return user;
 	}
